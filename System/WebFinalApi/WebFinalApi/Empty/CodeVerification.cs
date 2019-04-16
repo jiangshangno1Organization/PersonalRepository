52adf3297693aa82221f9ea18312905bc2b9f5db
zhuyidao <1044887175@qq.com>
2019-04-16 16:53:04 +0800

    no message

---
+++ System/WebFinalApi/WebFinalApi/Empty/CodeVerification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFinalApi.Empty
+{
+    public class CodeVerification
+    {
+        public int id { get; set; }
+        public string values { get; set; }
+        public string ifSend { get; set; }
+        public string status { get; set; }
+        public DateTime initTime { get; set; }
+        public DateTime sendTime { get; set; }
+        public string type { get; set; }
+        public int failtime { get; set; }
+    }
+}
--- System/WebFinalApi/WebFinalApi/Empty/User.cs
+++ System/WebFinalApi/WebFinalApi/Empty/User.cs
@@ -7,6 +7,14 @@ namespace WebFinalApi.Empty
 {
     public class User
     {
-
+        public string userId { get; set; }
+        public string userName { get; set; }
+        public string mobile { get; set; }
+        public string password { get; set; }
+        public DateTime updateTime { get; set; }
+        public string status { get; set; }
+        public string memo { get; set; }
+        public string ifdel { get; set; }
+        public DateTime inittime { get; set; }
     }
 }
